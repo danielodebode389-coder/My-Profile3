99e835260b05614471474a8f546b3f0c4498468e
Daniel <danielodebode389@gmail.com>
2025-12-10 12:31:29 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//Hero Button
+document.getElementById("hireBtn").addEventListener("click", () => {
+    alert("Thank you for your interest. I will reach out to you soon");
+});
+
+// Contact Form Valdation
+document.getElementById("contactForm").addEventListener("submit", function(e){
+    e.preventDefault();
+    
+    let name = document.getElementById("contactName").value;
+    let email = document.getElementById("contactEmail").value;
+    let msg = document.getElementById("message").value;
+    let feedback = document.getElementById("feedback");
+
+    if(name === "" || email === "" || msg === ""){
+        feedback.textContent = "All fields are required!";
+        feedback.style.color = "red";
+        return;
+    }
+
+    feedback.textContent = "Message sent succesfully!";
+});
